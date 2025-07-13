36e0d8423406a11d9c14fe004d3eaa0c607c4986
azizbek.isakov <azizbek.isakov@fido.uz>
2025-07-13 11:12:13 +0500

    Initial Commit

---
+++ src/test/java/uz/expense/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package uz.expense;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
